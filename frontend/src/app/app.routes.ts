a9a34b1dbd708e11e37605937c4e9b6733e90e81
Térence <laurent.terence@gmail.com>
2025-03-23 14:15:34 +0100

    alias + ng-shadcn

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from '@app/login/login.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
