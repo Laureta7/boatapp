72c48e43119c02a1e15d717c0c2c7d51fa0b5af8
Térence <laurent.terence@gmail.com>
2025-03-23 14:08:17 +0100

    Login component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
