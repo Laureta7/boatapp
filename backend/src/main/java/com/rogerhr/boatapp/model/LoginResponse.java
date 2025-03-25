67fc990794a9753742adcdba6a7329e63f8ba681
Térence <laurent.terence@gmail.com>
2025-03-25 13:16:29 +0100

    Login response to record

--- backend/src/main/java/com/rogerhr/boatapp/model/LoginResponse.java
+++ backend/src/main/java/com/rogerhr/boatapp/model/LoginResponse.java
@@ -1,17 +1,4 @@
 package com.rogerhr.boatapp.model;
 
-public class LoginResponse {
-  private String message; // Changed from token to message
-
-  public LoginResponse(String message) {
-    this.message = message;
-  }
-
-  public String getMessage() {
-    return message;
-  }
-
-  public void setMessage(String message) {
-    this.message = message;
-  }
+public record LoginResponse(String message) {
 }
